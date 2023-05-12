7fe10d1ac790ac4975f57ae279d6794f615e76eb
aelt95 <anthonylopez1230@gmail.com>
2023-05-12 23:37:04 +0200

    Full Create Post

--- front-end/src/components/FormField.jsx
+++ front-end/src/components/FormField.jsx
@@ -28,16 +28,17 @@ function FormField({
             Surprise me
           </button>
         )}
-        <input
-          type={type}
-          id={name}
-          placeholder={placeholder}
-          value={value}
-          onChange={handleChange}
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#6469ff] focus:border-[#6469ff] outline-none block w-full p-3"
-          required
-        />
       </div>
+      <input
+        type={type}
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#6469ff] focus:border-[#6469ff] outline-none block w-full p-3"
+        required
+      />
     </div>
   );
 }
