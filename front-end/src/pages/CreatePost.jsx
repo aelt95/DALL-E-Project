84263ede318ea9a35e15bd7d8f4ba2e343078740
aelt95 <anthonylopez1230@gmail.com>
2023-05-13 23:55:01 +0200

    code review

--- front-end/src/pages/CreatePost.jsx
+++ front-end/src/pages/CreatePost.jsx
@@ -20,7 +20,6 @@ function CreatePost() {
           body: JSON.stringify({ prompt: form.prompt }),
         });
         const data = await response.json();
-        console.log(data);
         setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
       } catch (error) {
         alert(error);
@@ -31,7 +30,27 @@ function CreatePost() {
       alert("Plese enter a prompt");
     }
   };
-  const handleSubmit = () => {};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (form.prompt && form.photo) {
+      setLoading(true);
+      try {
+        const response = await fetch("http://localhost:8080/api/v1/post", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(form),
+        });
+        await response.json();
+        navigate("/");
+      } catch (error) {
+        alert(error);
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      alert("Please enter a prompt");
+    }
+  };
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
